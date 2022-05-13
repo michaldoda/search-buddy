d2649dd6e58ffd8b1b9861ec27930f7a420c8bae
michaldoda <michael.johnny.b@gmail.com>
2022-05-13 22:55:19 +0200

    chore: setup minimal configuration

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: 'development.js',
+        path: path.resolve(__dirname, 'dev'),
     },
 };
