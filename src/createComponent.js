b6ce8bab4b892e6e098cfa45232ba2b88cb7e85a
michaldoda <michael.johnny.b@gmail.com>
2022-05-18 23:24:17 +0200

    chore: update styles

--- src/createComponent.js
+++ src/createComponent.js
@@ -37,7 +37,7 @@ const createComponent = (options) => {
     const searchButtonElement = document.createElement("button");
     searchButtonElement.classList.add("AwesomePlugin-form-wrapper-fields-wrapper-search-button");
     searchButtonElement.innerHTML = "&#128269;";
-    searchButtonElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" style='color: #4947ff; margin-top: 4px; height: 20px; width: 20px' fill=\"currentColor\" class=\"bi bi-search\" viewBox=\"0 0 16 16\"> <path d=\"M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z\"/> </svg>";
+    searchButtonElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" style='color: rgba(0,0,0,0.4); margin-top: 4px; height: 20px; width: 20px' fill=\"currentColor\" class=\"bi bi-search\" viewBox=\"0 0 16 16\"> <path d=\"M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z\"/> </svg>";
     searchButtonElement.type = "submit";
     fieldsWrapper.appendChild(searchButtonElement)
 
