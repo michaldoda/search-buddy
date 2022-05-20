4a8fb969543e15a70d7780a418173193462cfa2b
michaldoda <michael.johnny.b@gmail.com>
2022-05-20 10:46:40 +0200

    chore: update tests

--- tests/SearchBuddy.test.js
+++ tests/SearchBuddy.test.js
@@ -4,9 +4,20 @@
 import {SearchBuddy} from "../src/SearchBuddy";
 
 
-test("Initialize plugin", () => {
-    let searchBuddy = SearchBuddy({
+test("Base tests", () => {
+    let {
+        container,
+        show,
+        hide,
+    } = SearchBuddy({
         mode: "local",
         items: [],
     });
+
+    expect(document.querySelector(`.${container.className}`)).toBe(container);
+    expect(container.style.display).toBe("");
+    show();
+    expect(container.style.display).toBe("inline-block");
+    hide();
+    expect(container.style.display).toBe("none");
 });
